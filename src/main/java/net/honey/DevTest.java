3923cbb4502914cebf86f6175c4f996838c30be4
PurpleCloud <wtf2.ilja@gmail.com>
2022-11-18 14:13:57 +0000

    Made progress to completing the game

--- src/main/java/net/honey/DevTest.java
+++ src/main/java/net/honey/DevTest.java
@@ -1,26 +1,18 @@
 package net.honey;
 
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-
+import org.junit.Test;
 public class DevTest {
     public static void main(String[] args) {
-        testReading();
+        System.out.println("Hello World");
     }
+    @Test
     //Test that the Json reading works and make sure that there is no invalid types in json files
-    private static void testReading(){
-        try {
-            CardDataReader cardDataReader = new CardDataReader("CardDef.json");
-            CardPOJO card;
-            for (int itemId= 0; itemId < cardDataReader.getLength();itemId++) {
-                card= cardDataReader.getFromId(itemId);
-            }
-            cardDataReader.printPretty();
-
-        } catch (Exception e) {
-            System.out.println("ERROR: Json parsing failed, json file may be corrupted");
-            e.printStackTrace();
+    public void testJson(){
+        CardDataReader cardDataReader = new CardDataReader("CardDef.json");
+        CardPOJO card;
+        for (int itemId= 0; itemId < cardDataReader.getLength();itemId++) {
+            card= cardDataReader.getFromId(itemId);
         }
-
+        cardDataReader.printPretty();
     }
 }
